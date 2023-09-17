b1d80403648a13d61e7e5bfe62f65e47d9aa8e9d
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-18 05:25:58 +0600

    test widget updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:learn_provider/main.dart';
+import 'package:learn_provider/learn_provider.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LearnProvider());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
