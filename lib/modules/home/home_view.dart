40080a8f27ef24b3fee08b551f063efc018f57c2
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-18 05:17:59 +0600

    home view added with counter functionality with provider basics

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'home_provider.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    HomeProvider readHomeProvider = context.read<HomeProvider>();
+    HomeProvider homeProvider = Provider.of<HomeProvider>(context);
+    debugPrint('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${homeProvider.count}',
+              key: const Key('counterState'),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => readHomeProvider.increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
