4c457ec4130995b0314614961d7a38e2e87fbcc0
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-18 05:42:55 +0600

    switch component name changed

--- lib/components/nswitch.dart
+++ lib/components/nswitch.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-Widget CustomSwitch({required bool value, required Function(bool) onChanged}) {
+Widget NSwitch({required bool value, required Function(bool) onChanged}) {
   return Switch(value: value, onChanged: onChanged);
 }
