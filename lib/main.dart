bc604298681d70fb370e631f5855d8031ba905e0
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-18 04:57:41 +0600

    home provider added in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:learn_provider/learn_provider.dart';
+import 'package:learn_provider/modules/home/home_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const LearnProvider());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeProvider()),
+      ],
+      child: const LearnProvider(),
+    ),
+  );
 }
