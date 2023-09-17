8d3d7cd444d6069e87e255eb5f47290ec2f614be
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-18 05:50:45 +0600

    switch related functionality added in home provider

--- lib/modules/home/home_provider.dart
+++ lib/modules/home/home_provider.dart
@@ -2,11 +2,17 @@ import 'package:flutter/foundation.dart';
 
 class HomeProvider with ChangeNotifier, DiagnosticableTreeMixin {
   int _count = 0;
+  bool switchValue = false;
 
   int get count => _count;
 
   void increment() {
     _count++;
+    notifyListeners();
+  }
+
+  void changeSwitchValue(bool value) {
+    switchValue = value;
     notifyListeners();
   }
 
