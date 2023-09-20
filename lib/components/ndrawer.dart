077432bc3060a55af81c2853d97ca1332ea05932
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-21 00:56:51 +0600

    app logo added on drawer

--- lib/components/ndrawer.dart
+++ lib/components/ndrawer.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../config/app_assets.dart';
+
 class NDrawer extends StatelessWidget {
   const NDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer();
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DrawerHeader(child: Image.asset(AppAssets.ASSET_APP_LOGO)),
+          Expanded(
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(0),
+              children: const [
+                Divider(color: Colors.grey),
+              ],
+            ),
+          ),
+          const Divider(),
+        ],
+      ),
+    );
   }
 }
