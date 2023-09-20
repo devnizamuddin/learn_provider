6a055dca3330e9d68440c8819afcd94140bc0b02
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-21 04:52:32 +0600

    favorite provider added on main

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:learn_provider/learn_provider.dart';
 import 'package:learn_provider/modules/home/home_provider.dart';
 import 'package:provider/provider.dart';
 
+import 'modules/favorites/favorites_provider.dart';
+
 void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => HomeProvider()),
+        ChangeNotifierProvider(create: (_) => FavoritesProvider()),
       ],
       child: const LearnProvider(),
     ),
