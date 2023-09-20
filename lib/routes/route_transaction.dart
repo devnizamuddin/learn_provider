02247c7acc57e8bde4e4db02f9022aa9b7e5b4a7
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-21 04:25:47 +0600

    pushNamed function added on RouteTransaction

--- lib/routes/route_transaction.dart
+++ lib/routes/route_transaction.dart
@@ -11,4 +11,13 @@ class RouteTransaction {
     return Navigator.pushReplacementNamed(buildContext, routeName,
         result: result, arguments: arguments);
   }
+
+  static Future<Object> pushNamed<T extends Object?, TO extends Object?>({
+    required BuildContext buildContext,
+    required String routeName,
+    TO? result,
+    Object? arguments,
+  }) async {
+    return Navigator.pushNamed(buildContext, routeName, arguments: arguments);
+  }
 }
