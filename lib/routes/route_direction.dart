f1f5cf871e0171d2a1c3958c82471f375550e325
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-21 04:14:36 +0600

    name change of RouteManageer files, class and variable

--- lib/routes/route_direction.dart
+++ lib/routes/route_direction.dart
@@ -4,8 +4,8 @@ import 'package:learn_provider/routes/route_names.dart';
 import 'package:learn_provider/modules/home/home_screen.dart';
 import '../modules/favorites/favorites_screen.dart';
 
-class RouteManager {
-  static Map<String, Widget Function(BuildContext)> routes =
+class RouteDirection {
+  static Map<String, Widget Function(BuildContext)> routeMap =
       <String, WidgetBuilder>{
     RouteNames.HOME_SCREEN: (_) => const HomeScreen(),
     RouteNames.FAVORITES_SCREEN: (_) => const FavoritesScreen(),
