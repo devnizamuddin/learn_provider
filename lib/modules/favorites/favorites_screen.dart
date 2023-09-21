cbbf50acfbdcdb4b90ee1ea7e9fc84dbb3d0b5a8
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-21 08:33:40 +0600

    FavoritesScreen designed and added data functionality from FavoritesProvider

--- lib/modules/favorites/favorites_screen.dart
+++ lib/modules/favorites/favorites_screen.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:learn_provider/modules/favorites/favorites_provider.dart';
+import 'package:provider/provider.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final provider = Provider.of<FavoritesProvider>(context, listen: false);
+    debugPrint('FavoritesScreen building');
     return Scaffold(
-      appBar: AppBar(title: const Text('Favorites')),
+      backgroundColor: const Color.fromARGB(255, 235, 235, 235),
+      appBar: AppBar(
+        title: const Text('Favorites'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (buildContext, index) {
+              debugPrint('Favorites ListItem building');
+              return Consumer<FavoritesProvider>(
+                  builder: (buildContext, provider, widget) {
+                debugPrint('Favorites Card building');
+                return Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Item Number : ${index + 1}'),
+                      InkWell(
+                        onTap: () =>
+                            provider.onTapFavoritesIcon(itemIndex: index),
+                        child: Icon(
+                          provider.favoriteItems.contains(index)
+                              ? Icons.favorite
+                              : Icons.favorite_border,
+                          color: Colors.amber,
+                        ),
+                      )
+                    ],
+                  ),
+                ));
+              });
+            }),
+      ),
     );
   }
 }
