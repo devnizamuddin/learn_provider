3c0745147091ef8ee774a841b1813da323c7b03b
nizamuddinshamrat <nshamrat7@gmail.com>
2023-09-21 08:31:15 +0600

    variable and functionality added on FavoritesProvider

--- lib/modules/favorites/favorites_provider.dart
+++ lib/modules/favorites/favorites_provider.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/foundation.dart';
 
-class FavoritesProvider with ChangeNotifier, DiagnosticableTreeMixin {}
+class FavoritesProvider with ChangeNotifier, DiagnosticableTreeMixin {
+  List<int> favoriteItems = [];
+
+  onTapFavoritesIcon({required int itemIndex}) {
+    favoriteItems.contains(itemIndex)
+        ? favoriteItems.remove(itemIndex)
+        : favoriteItems.add(itemIndex);
+    notifyListeners();
+  }
+}
